e8aa7b1ebbad0e35c11a1189fa62405f5740d9dd
xoroperatorxd <barik2k@gmail.com>
2024-03-01 00:46:00 +0530

    projectile cooldown added to code

--- Assets/MetroidvaniaController/Scripts/Player/Attack.cs
+++ Assets/MetroidvaniaController/Scripts/Player/Attack.cs
@@ -1,70 +1,73 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
-	public float dmgValue = 4;
-	public GameObject throwableObject;
-	public Transform attackCheck;
-	private Rigidbody2D m_Rigidbody2D;
-	public Animator animator;
-	public bool canAttack = true;
-	public bool isTimeToCheck = false;
+    public float dmgValue = 4;
+    public GameObject throwableObject;
+    public Transform attackCheck;
+    private Rigidbody2D m_Rigidbody2D;
+    public Animator animator;
+    public bool canAttack = true;
+    public bool isTimeToCheck = false;
 
-	public GameObject cam;
+    public GameObject cam;
 
-	private void Awake()
-	{
-		m_Rigidbody2D = GetComponent<Rigidbody2D>();
-	}
+    // Fire rate variables
+    public float throwCooldown = 0.5f; // Time between each throwable object throw
+    public float nextThrowTime = 0f;
 
-	// Start is called before the first frame update
-	void Start()
+    private void Awake()
     {
-        
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-		if (Input.GetKeyDown(KeyCode.X) && canAttack)
-		{
-			canAttack = false;
-			animator.SetBool("IsAttacking", true);
-			StartCoroutine(AttackCooldown());
-		}
+        if (Input.GetKeyDown(KeyCode.X) && canAttack)
+        {
+            canAttack = false;
+            animator.SetBool("IsAttacking", true);
+            StartCoroutine(AttackCooldown());
+        }
 
-		if (Input.GetKeyDown(KeyCode.V))
-		{
-			GameObject throwableWeapon = Instantiate(throwableObject, transform.position + new Vector3(transform.localScale.x * 0.5f,+0.1f), Quaternion.identity) as GameObject; 
-			Vector2 direction = new Vector2(transform.localScale.x, 0);
-			throwableWeapon.GetComponent<ThrowableWeapon>().direction = direction; 
-			throwableWeapon.name = "ThrowableWeapon";
-		}
-	}
+        if (Input.GetKeyDown(KeyCode.V) && Time.time >= nextThrowTime)
+        {
+            nextThrowTime = Time.time + throwCooldown; // Update next throw time
+            ThrowObject();
+        }
+    }
 
-	IEnumerator AttackCooldown()
-	{
-		yield return new WaitForSeconds(0.25f);
-		canAttack = true;
-	}
+    void ThrowObject()
+    {
+        GameObject throwableWeapon = Instantiate(throwableObject, transform.position + new Vector3(transform.localScale.x * 0.5f, +0.1f), Quaternion.identity) as GameObject;
+        Vector2 direction = new Vector2(transform.localScale.x, 0);
+        throwableWeapon.GetComponent<ThrowableWeapon>().direction = direction;
+        throwableWeapon.name = "ThrowableWeapon";
+    }
 
-	public void DoDashDamage()
-	{
-		dmgValue = Mathf.Abs(dmgValue);
-		Collider2D[] collidersEnemies = Physics2D.OverlapCircleAll(attackCheck.position, 0.9f);
-		for (int i = 0; i < collidersEnemies.Length; i++)
-		{
-			if (collidersEnemies[i].gameObject.tag == "Enemy")
-			{
-				if (collidersEnemies[i].transform.position.x - transform.position.x < 0)
-				{
-					dmgValue = -dmgValue;
-				}
-				collidersEnemies[i].gameObject.SendMessage("ApplyDamage", dmgValue);
-				CameraFollow.instance.ShakeCamera();
-			}
-		}
-	}
+    IEnumerator AttackCooldown()
+    {
+        yield return new WaitForSeconds(0.25f);
+        canAttack = true;
+    }
+
+    public void DoDashDamage()
+    {
+        dmgValue = Mathf.Abs(dmgValue);
+        Collider2D[] collidersEnemies = Physics2D.OverlapCircleAll(attackCheck.position, 0.9f);
+        for (int i = 0; i < collidersEnemies.Length; i++)
+        {
+            if (collidersEnemies[i].gameObject.tag == "Enemy")
+            {
+                if (collidersEnemies[i].transform.position.x - transform.position.x < 0)
+                {
+                    dmgValue = -dmgValue;
+                }
+                collidersEnemies[i].gameObject.SendMessage("ApplyDamage", dmgValue);
+                CameraFollow.instance.ShakeCamera();
+            }
+        }
+    }
 }
